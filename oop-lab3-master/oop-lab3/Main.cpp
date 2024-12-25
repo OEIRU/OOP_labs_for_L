598a8b0b9115210614bee9c8fe0023c2b53efb60
VKostovsky <SpasiboMiryZaZemfiry@gmail.com>
2024-12-25 15:59:38 +0700

    add labs

---
+++ oop-lab3-master/oop-lab3/Main.cpp
@@ -0,0 +1,16 @@
+#define CATCH_CONFIG_RUNNER
+#include "catch.hpp"
+#include "Interface.h"
+
+
+int main(int argc, char** argv)
+{
+	setlocale(LC_ALL, "ru");
+
+	start();
+
+	//int result = Catch::Session().run(argc, argv);
+	//return result;
+
+	return 0;
+}
